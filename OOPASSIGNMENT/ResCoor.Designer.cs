917e48b2960b76e83c3ef28cd63b4779a6424ff8
Harol D. Barry <kokoaungkaungkhant@gmail.com>
2025-02-28 04:24:35 +0800

    Reservation Coordinator

---
+++ OOPASSIGNMENT/ResCoor.Designer.cs
@@ -0,0 +1,341 @@
+﻿namespace OOPASSIGNMENT
+{
+    partial class ResCoor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ResCoor));
+            this.label4 = new System.Windows.Forms.Label();
+            this.pBlogo = new System.Windows.Forms.PictureBox();
+            this.lB1Head = new System.Windows.Forms.Label();
+            this.lB2Head = new System.Windows.Forms.Label();
+            this.lB3Head = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.BtMRes = new System.Windows.Forms.Button();
+            this.BtURes = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.buComp = new System.Windows.Forms.Button();
+            this.buCfm = new System.Windows.Forms.Button();
+            this.BtFed = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.buReply = new System.Windows.Forms.Button();
+            this.buView = new System.Windows.Forms.Button();
+            this.BtUPf = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buEdit = new System.Windows.Forms.Button();
+            this.buAdd = new System.Windows.Forms.Button();
+            this.buDel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pBlogo)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.label4.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.label4.Location = new System.Drawing.Point(3, 9);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(1502, 212);
+            this.label4.TabIndex = 35;
+            // 
+            // pBlogo
+            // 
+            this.pBlogo.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.pBlogo.Image = ((System.Drawing.Image)(resources.GetObject("pBlogo.Image")));
+            this.pBlogo.Location = new System.Drawing.Point(8, 11);
+            this.pBlogo.Margin = new System.Windows.Forms.Padding(6);
+            this.pBlogo.Name = "pBlogo";
+            this.pBlogo.Size = new System.Drawing.Size(234, 210);
+            this.pBlogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pBlogo.TabIndex = 36;
+            this.pBlogo.TabStop = false;
+            // 
+            // lB1Head
+            // 
+            this.lB1Head.AutoSize = true;
+            this.lB1Head.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.lB1Head.Font = new System.Drawing.Font("Modern No. 20", 25.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lB1Head.Location = new System.Drawing.Point(574, 30);
+            this.lB1Head.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lB1Head.Name = "lB1Head";
+            this.lB1Head.Size = new System.Drawing.Size(392, 71);
+            this.lB1Head.TabIndex = 37;
+            this.lB1Head.Text = "Foodie Point";
+            // 
+            // lB2Head
+            // 
+            this.lB2Head.BackColor = System.Drawing.Color.Black;
+            this.lB2Head.Location = new System.Drawing.Point(335, 115);
+            this.lB2Head.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lB2Head.Name = "lB2Head";
+            this.lB2Head.Size = new System.Drawing.Size(900, 12);
+            this.lB2Head.TabIndex = 38;
+            this.lB2Head.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lB3Head
+            // 
+            this.lB3Head.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.lB3Head.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lB3Head.Location = new System.Drawing.Point(282, 149);
+            this.lB3Head.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lB3Head.Name = "lB3Head";
+            this.lB3Head.Size = new System.Drawing.Size(1032, 38);
+            this.lB3Head.TabIndex = 39;
+            this.lB3Head.Text = "Bringing premium cuisines from all around the world straight to your table";
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.label5.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.label5.Location = new System.Drawing.Point(3, 221);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(239, 981);
+            this.label5.TabIndex = 40;
+            // 
+            // BtMRes
+            // 
+            this.BtMRes.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BtMRes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtMRes.Font = new System.Drawing.Font("Modern No. 20", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtMRes.Location = new System.Drawing.Point(33, 281);
+            this.BtMRes.Margin = new System.Windows.Forms.Padding(4);
+            this.BtMRes.Name = "BtMRes";
+            this.BtMRes.Size = new System.Drawing.Size(180, 188);
+            this.BtMRes.TabIndex = 41;
+            this.BtMRes.Text = "Manage Reservation";
+            this.BtMRes.UseVisualStyleBackColor = false;
+            // 
+            // BtURes
+            // 
+            this.BtURes.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BtURes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtURes.Font = new System.Drawing.Font("Modern No. 20", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtURes.Location = new System.Drawing.Point(33, 518);
+            this.BtURes.Margin = new System.Windows.Forms.Padding(4);
+            this.BtURes.Name = "BtURes";
+            this.BtURes.Size = new System.Drawing.Size(180, 188);
+            this.BtURes.TabIndex = 43;
+            this.BtURes.Text = "Update Reservation";
+            this.BtURes.UseVisualStyleBackColor = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.panel2.Controls.Add(this.buComp);
+            this.panel2.Controls.Add(this.buCfm);
+            this.panel2.Location = new System.Drawing.Point(221, 552);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(273, 127);
+            this.panel2.TabIndex = 43;
+            this.panel2.Visible = false;
+            // 
+            // buComp
+            // 
+            this.buComp.Font = new System.Drawing.Font("Modern No. 20", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buComp.Location = new System.Drawing.Point(13, 74);
+            this.buComp.Margin = new System.Windows.Forms.Padding(4);
+            this.buComp.Name = "buComp";
+            this.buComp.Size = new System.Drawing.Size(246, 37);
+            this.buComp.TabIndex = 1;
+            this.buComp.Text = "Completed";
+            this.buComp.UseVisualStyleBackColor = true;
+            // 
+            // buCfm
+            // 
+            this.buCfm.Font = new System.Drawing.Font("Modern No. 20", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buCfm.Location = new System.Drawing.Point(13, 19);
+            this.buCfm.Margin = new System.Windows.Forms.Padding(4);
+            this.buCfm.Name = "buCfm";
+            this.buCfm.Size = new System.Drawing.Size(246, 37);
+            this.buCfm.TabIndex = 0;
+            this.buCfm.Text = "Confirmed";
+            this.buCfm.UseVisualStyleBackColor = true;
+            // 
+            // BtFed
+            // 
+            this.BtFed.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BtFed.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtFed.Font = new System.Drawing.Font("Modern No. 20", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtFed.Location = new System.Drawing.Point(33, 753);
+            this.BtFed.Margin = new System.Windows.Forms.Padding(4);
+            this.BtFed.Name = "BtFed";
+            this.BtFed.Size = new System.Drawing.Size(180, 188);
+            this.BtFed.TabIndex = 44;
+            this.BtFed.Text = "Feedback";
+            this.BtFed.UseVisualStyleBackColor = false;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.panel3.Controls.Add(this.buReply);
+            this.panel3.Controls.Add(this.buView);
+            this.panel3.Location = new System.Drawing.Point(221, 789);
+            this.panel3.Margin = new System.Windows.Forms.Padding(4);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(273, 127);
+            this.panel3.TabIndex = 44;
+            this.panel3.Visible = false;
+            // 
+            // buReply
+            // 
+            this.buReply.Font = new System.Drawing.Font("Modern No. 20", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buReply.Location = new System.Drawing.Point(13, 74);
+            this.buReply.Margin = new System.Windows.Forms.Padding(4);
+            this.buReply.Name = "buReply";
+            this.buReply.Size = new System.Drawing.Size(246, 37);
+            this.buReply.TabIndex = 1;
+            this.buReply.Text = "Reply";
+            this.buReply.UseVisualStyleBackColor = true;
+            // 
+            // buView
+            // 
+            this.buView.Font = new System.Drawing.Font("Modern No. 20", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buView.Location = new System.Drawing.Point(13, 19);
+            this.buView.Margin = new System.Windows.Forms.Padding(4);
+            this.buView.Name = "buView";
+            this.buView.Size = new System.Drawing.Size(246, 37);
+            this.buView.TabIndex = 0;
+            this.buView.Text = "View";
+            this.buView.UseVisualStyleBackColor = true;
+            this.buView.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // BtUPf
+            // 
+            this.BtUPf.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BtUPf.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtUPf.Font = new System.Drawing.Font("Modern No. 20", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtUPf.Location = new System.Drawing.Point(33, 998);
+            this.BtUPf.Margin = new System.Windows.Forms.Padding(4);
+            this.BtUPf.Name = "BtUPf";
+            this.BtUPf.Size = new System.Drawing.Size(180, 188);
+            this.BtUPf.TabIndex = 45;
+            this.BtUPf.Text = "Update Profile";
+            this.BtUPf.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.panel1.Controls.Add(this.buDel);
+            this.panel1.Controls.Add(this.buEdit);
+            this.panel1.Controls.Add(this.buAdd);
+            this.panel1.Location = new System.Drawing.Point(221, 286);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(273, 183);
+            this.panel1.TabIndex = 44;
+            this.panel1.Visible = false;
+            // 
+            // buEdit
+            // 
+            this.buEdit.Font = new System.Drawing.Font("Modern No. 20", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buEdit.Location = new System.Drawing.Point(13, 74);
+            this.buEdit.Margin = new System.Windows.Forms.Padding(4);
+            this.buEdit.Name = "buEdit";
+            this.buEdit.Size = new System.Drawing.Size(246, 37);
+            this.buEdit.TabIndex = 1;
+            this.buEdit.Text = "Edit";
+            this.buEdit.UseVisualStyleBackColor = true;
+            // 
+            // buAdd
+            // 
+            this.buAdd.Font = new System.Drawing.Font("Modern No. 20", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buAdd.Location = new System.Drawing.Point(13, 19);
+            this.buAdd.Margin = new System.Windows.Forms.Padding(4);
+            this.buAdd.Name = "buAdd";
+            this.buAdd.Size = new System.Drawing.Size(246, 37);
+            this.buAdd.TabIndex = 0;
+            this.buAdd.Text = "Add";
+            this.buAdd.UseVisualStyleBackColor = true;
+            // 
+            // buDel
+            // 
+            this.buDel.Font = new System.Drawing.Font("Modern No. 20", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buDel.Location = new System.Drawing.Point(13, 133);
+            this.buDel.Margin = new System.Windows.Forms.Padding(4);
+            this.buDel.Name = "buDel";
+            this.buDel.Size = new System.Drawing.Size(246, 37);
+            this.buDel.TabIndex = 2;
+            this.buDel.Text = "Delete";
+            this.buDel.UseVisualStyleBackColor = true;
+            // 
+            // ResCoor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1506, 1199);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.BtUPf);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.BtFed);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.BtURes);
+            this.Controls.Add(this.BtMRes);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lB3Head);
+            this.Controls.Add(this.lB2Head);
+            this.Controls.Add(this.lB1Head);
+            this.Controls.Add(this.pBlogo);
+            this.Controls.Add(this.label4);
+            this.Name = "ResCoor";
+            this.Text = "ResCoor";
+            ((System.ComponentModel.ISupportInitialize)(this.pBlogo)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pBlogo;
+        private System.Windows.Forms.Label lB1Head;
+        private System.Windows.Forms.Label lB2Head;
+        private System.Windows.Forms.Label lB3Head;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button BtMRes;
+        private System.Windows.Forms.Button BtURes;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button buComp;
+        private System.Windows.Forms.Button buCfm;
+        private System.Windows.Forms.Button BtFed;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button buReply;
+        private System.Windows.Forms.Button buView;
+        private System.Windows.Forms.Button BtUPf;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buDel;
+        private System.Windows.Forms.Button buEdit;
+        private System.Windows.Forms.Button buAdd;
+    }
+}
